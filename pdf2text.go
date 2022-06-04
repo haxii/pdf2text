6541062f42ccdf86810cb667b91e894c468fef41
Zichao LI <cszichao@gmail.com>
2022-06-05 00:28:59 +0800

    supports for crop

--- pdf2text.go
+++ pdf2text.go
@@ -20,6 +20,12 @@ func convertPDF(ctx *fasthttp.RequestCtx) {
 		return
 	}
 	tmpFileName := fmt.Sprintf("/tmp/pdf2text-%s.tmp", xid.New().String())
+	query := ctx.QueryArgs()
+	x := string(query.Peek("x"))
+	y := string(query.Peek("y"))
+	w := string(query.Peek("w"))
+	h := string(query.Peek("h"))
+
 	bodyBytes := ctx.PostBody()
 	err := ioutil.WriteFile(tmpFileName, bodyBytes, 0600)
 	if err != nil {
@@ -30,7 +36,22 @@ func convertPDF(ctx *fasthttp.RequestCtx) {
 		_ = os.Remove(tmpFileName)
 	}()
 
-	body, err := exec.Command("pdftotext", "-nopgbrk", "-enc", "UTF-8", tmpFileName, "-").Output()
+	args := make([]string, 0)
+	if len(x) > 0 {
+		args = append(args, "-x", x)
+	}
+	if len(y) > 0 {
+		args = append(args, "-y", y)
+	}
+	if len(w) > 0 {
+		args = append(args, "-W", w)
+	}
+	if len(h) > 0 {
+		args = append(args, "-H", h)
+	}
+	args = append(args, "-nopgbrk", "-enc", "UTF-8", tmpFileName, "-")
+
+	body, err := exec.Command("pdftotext", args...).Output()
 	if err != nil {
 		log.Printf("pdftotext error: %s", err)
 		ctx.Error(err.Error(), 500)
